9d18609e2190bbddef29f8aceeea53aa3bfce7aa
Loay Idwan <contact@loayidwan.com>
2024-10-07 10:12:10 +0000

    Linked btn to function

---
+++ website/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".search-btn");
+
+btn.addEventListener("click", () => {
+    const div = document.querySelector(".test");
+    div.textContent = "THIS IS WORKING!!";
+});
