29cbf333b5e70f4c9bb70a9a08ab2e26776ca55e
Loay Idwan <contact@loayidwan.com>
2024-10-07 10:16:06 +0000

    Made into named function

--- website/script.js
+++ website/script.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector(".search-btn");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", Search);
+
+
+function Search() {
     const div = document.querySelector(".test");
     div.textContent = "THIS IS WORKING!!";
-});
+}
