078a43e1c8cf1ef44e46bfdd91b56dd7bcf0fe0b
Loay Idwan <contact@loayidwan.com>
2024-10-07 10:42:44 +0000

    Testing funct.

--- website/script.js
+++ website/script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector(".search-btn");
 
-btn.addEventListener("click", Search);
+btn.addEventListener("click", search);
 
 
-function Search() {
+async function search() {
+    const query = document.querySelector(".search-query").value;
+
     const div = document.querySelector(".test");
-    div.textContent = "THIS IS WORKING!!";
+    div.textContent = query;
 }
