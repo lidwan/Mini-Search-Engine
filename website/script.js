159b5f7be2ce424a7a248e5b626ba0802606a273
Loay Idwan <contact@loayidwan.com>
2024-10-09 08:51:35 +0000

    Added results handling in js

--- website/script.js
+++ website/script.js
@@ -5,7 +5,15 @@ btn.addEventListener("click", search);
 
 async function search() {
     const query = document.querySelector(".search-query").value;
+    const response = await fetch('#');
+    const results = await response.json();
 
-    const div = document.querySelector(".test");
-    div.textContent = query;
+    const resultsDiv = document.querySelector(".results");
+    resultsDiv.innerHTML = '';
+    
+    results.forEach(article => {
+        const articleDiv = document.createElement('div');
+        articleDiv.innerHTML = `<h2>${article.title}</h2><p>${article.content}</p><a href="${article.link}" target="_blank">Read more</a>`;
+        resultsDiv.appendChild(articleDiv);
+    });
 }
